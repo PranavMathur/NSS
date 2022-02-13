f16bccffb291891d7a3492cf91f09bfb698855e5
Pranav Mathur <pranavmathur99@gmail.com>
2022-02-12 20:31:24 -0800

    Add main function

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import lark
+import argparse
 
 grammar = """
 start: sheet
@@ -16,3 +17,13 @@ rule: ""
 %ignore WS_INLINE
 %ignore WS
 """
+
+def main():
+    cli_parser = argparse.ArgumentParser(prog='nss')
+    parser.add_argument('source', type=argparse.FileType('r'))
+    parser.add_argument('target', nargs='?',
+                        type=argparse.FileType('w'), default=sys.stdout)
+    args = cli_parser.parse_args()
+
+if __name__ == '__main__':
+    main()
