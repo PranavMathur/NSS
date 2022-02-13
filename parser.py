6eb1c6009b3cd080bed7f7a14dfdf4630f70f3da
Pranav Mathur <pranavmathur99@gmail.com>
2022-02-12 20:28:05 -0800

    Add lark imports

--- parser.py
+++ parser.py
@@ -6,4 +6,13 @@ start: sheet
 sheet: rule*
 
 rule: ""
+
+%import common.NUMBER
+%import common.ESCAPED_STRING
+%import common.CNAME -> NAME
+%import common.WS_INLINE
+%import common.WS
+
+%ignore WS_INLINE
+%ignore WS
 """
