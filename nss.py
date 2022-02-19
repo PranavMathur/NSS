35d8c30f03058302227b806db3c3a465bc95d66a
Pranav Mathur <pranavmathur99@gmail.com>
2022-02-19 15:23:00 -0800

    Work on grammar

--- nss.py
+++ nss.py
@@ -1,18 +1,28 @@
+import sys
 import lark
 import argparse
 
 grammar = """
-start: sheet
+?start: sheet
 
-sheet: rule*
+?sheet: rule*
 
-rule: ""
+rule: selector "{" block "}"
+
+selector: NAME
+
+block: declaration*
+
+declaration: rule
+           | NAME ":" NAME ";"
 
 %import common.NUMBER
 %import common.ESCAPED_STRING
 %import common.CNAME -> NAME
 %import common.WS_INLINE
 %import common.WS
+%import common.C_COMMENT
+%import common.CPP_COMMENT
 
 %ignore WS_INLINE
 %ignore WS
@@ -20,10 +30,14 @@ rule: ""
 
 def main():
     cli_parser = argparse.ArgumentParser(prog='nss')
-    parser.add_argument('source', type=argparse.FileType('r'))
-    parser.add_argument('target', nargs='?',
+    cli_parser.add_argument('source', type=argparse.FileType('r'))
+    cli_parser.add_argument('target', nargs='?',
                         type=argparse.FileType('w'), default=sys.stdout)
     args = cli_parser.parse_args()
+
+    parser = lark.Lark(grammar, parser='lalr')
+    tree = parser.parse(args.source.read())
+    print(tree.pretty())
 
 if __name__ == '__main__':
     main()
